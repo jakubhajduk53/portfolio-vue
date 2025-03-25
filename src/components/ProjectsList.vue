bf1ea7d1fde5f564c7bc7c7e3989837ff7aeee54
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-03-25 15:08:41 +0100

    feat: update site layout

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -9,18 +9,21 @@ import ProjectComponent from "./ProjectComponent.vue";
     name="Quiz App"
     description="Answer 10 various trivia questions"
     :icon="QuestionFilled"
+    customStyle="bg-[#DDA15E]"
   />
   <ProjectComponent
     href="https://weather-app-jakubhajduk53s-projects.vercel.app/"
     name="Weather App"
     description="Check the weather at any location"
     :icon="PartlyCloudy"
+    customStyle="bg-[#BC6C25]"
   />
   <ProjectComponent
     href="https://to-do-app-2025.vercel.app/"
     name="Todo App"
     description="Fill the list with your todo's"
     :icon="List"
+    customStyle="bg-[#606C38]"
   />
 </template>
 
